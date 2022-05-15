7a022ab37867b927e9f891a1559c387b1ab818f1
Jesse <jesseponce233@gmail.com>
2022-05-15 15:03:45 -0500

    second coommit making the mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,37 @@ var enemyAttack = 12;
 
 var fight = function() {
 window.alert("Welcome to Robot Gladiators!");
+
+enemyHealth = enemyHealth - playerAttack;
+
+console.log(
+    playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+// Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable 
+playerHealth = playerHealth - enemyAttack;
+
+// log a resulting message to the console so we know that it worked 
+console.log(
+    enemyName + " attacked " + playerName + ' . ' + playerName + " now has " + playerHealth + " health remaining "
+);
+
+// check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+} 
+else {
+    window.alert(playerName + " still has " + playerHealth + "health left.");
+}
+
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+} 
+else {
+    window.alert(enemyName + " still has " + enemyHealth + "health left.");
+}
+
 };
 
-fight();
+fight(); 
